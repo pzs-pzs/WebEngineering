a2353ef70b4b950db6877416e828cbeadea6314e
JunqingWang <junqing515@icloud.com>
2017-06-23 23:17:51 +0800

    FelixWangJQ

---
+++ task7/XDDLB/src/com/dlb/model/State.java
@@ -0,0 +1,17 @@
+package com.dlb.model;
+/**
+ * µÇÂ¼ºÍ×¢²á·µ»Ø×´Ì¬ÐÅÏ¢
+ * @author puzhengsong
+ *
+ */
+public class State {
+     	private String success;
+
+		public String getSuccess() {
+			return success;
+		}
+	
+		public void setSuccess(String success) {
+			this.success = success;
+		}
+}
