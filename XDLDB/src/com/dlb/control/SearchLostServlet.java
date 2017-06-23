b5ce9222fa263ffd14a4becd2b9e4df517e8375e
puzhengsong <1391663722@qq.com>
2017-06-23 11:40:25 +0800

    pzs

---
+++ XDLDB/src/com/dlb/control/SearchLostServlet.java
@@ -0,0 +1,57 @@
+package com.dlb.control;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dlb.model.RequestForm;
+import com.dlb.service.DataService;
+import com.dlb.service.impl.LostServeiceImpl;
+import com.dlb.utils.WebUtils;
+
+/**
+ * @author puzhengsong  
+ */
+@WebServlet("/SearchLostServlet")
+public class SearchLostServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		DataService service = new LostServeiceImpl();
+		RequestForm rform = WebUtils.requestToBean(request, RequestForm.class);
+		
+	    if(rform.getId()==null){
+			Writer out = response.getWriter();
+			String s = service.getDataToJson(rform);
+			//String s = "[{\"name\":\"pzs\",\"pass\":\"1234\"}]";
+			
+			out.write(s);
+			return ;
+	    }
+		
+		Writer out = response.getWriter();
+		String s = service.getDataToJsonByid(rform);
+		//String s = "[{\"name\":\"pzs\",\"pass\":\"1234\"}]";
+		
+		out.write(s);
+		return ;
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ XDLDB/src/com/dlb/control/StoreJobServlet.java
@@ -0,0 +1,54 @@
+package com.dlb.control;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSON;
+import com.dlb.model.EndData;
+import com.dlb.model.State;
+import com.dlb.service.DataService;
+import com.dlb.service.impl.JobServeiceImpl;
+import com.dlb.service.impl.LostServeiceImpl;
+import com.dlb.utils.WebUtils;
+
+/**
+ * @author puzhengsong  
+ */
+@WebServlet("/StoreJobServlet")
+public class StoreJobServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		EndData endData  = WebUtils.requestToBean(request, EndData.class);
+		;
+		Writer out = response.getWriter();
+		State s = new State();
+		DataService service = new JobServeiceImpl();
+		if(service.storeJsonDate(endData)){
+			s.setSuccess("1");
+			String json = JSON.toJSONString(s);
+			out.write(json);
+			return ;
+		}
+		s.setSuccess("0");
+		String json = JSON.toJSONString(s);
+		out.write(json);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
